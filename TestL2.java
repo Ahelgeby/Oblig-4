e25b3e2da0fdd6013261f8cddf2f7056686ab681
roberteh <roberteh@uio.no>
2024-03-26 10:52:40 +0100

    Ny testfil for Labyrint2

---
+++ TestL2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class TestL2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userInp;
+
+        do {
+
+
+            userInp = sc.nextLine();
+        }
+        while (!userInp.equals("-1"));
+
+
+
+
+
+
+        sc.close();
+    }
+    
+}
